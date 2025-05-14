36a1bb4bfe631ebda24e4709cc4497e59039c88d
arizaxel16 <arizaxel16@gmail.com>
2025-05-14 10:00:19 -0500

    Update service to use dtos

--- backend/src/main/java/com/backend/service/ImportService.java
+++ backend/src/main/java/com/backend/service/ImportService.java
@@ -1,11 +1,14 @@
 package com.backend.service;
 
+import com.backend.dto.ImportRequestDTO;
+import com.backend.dto.ImportResponseDTO;
 import com.backend.model.ImportRecord;
 import com.backend.repository.ImportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +16,30 @@ public class ImportService {
 
     private final ImportRepository importRepository;
 
-    public ImportRecord addImport(ImportRecord importData) {
-        return importRepository.save(importData);
+    public ImportResponseDTO addImport(ImportRequestDTO importRequestDTO) {
+        ImportRecord importData = new ImportRecord();
+        importData.setAmount(importRequestDTO.getAmount());
+        importData.setCityExit(importRequestDTO.getCityExit());
+        importData.setCityEntry(importRequestDTO.getCityEntry());
+
+        ImportRecord savedImport = importRepository.save(importData);
+        return convertToResponseDTO(savedImport);
     }
 
-    public List<ImportRecord> getAllImports() {
-        return importRepository.findAll();
+    public List<ImportResponseDTO> getAllImports() {
+        return importRepository.findAll().stream()
+                .map(this::convertToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    private ImportResponseDTO convertToResponseDTO(ImportRecord importData) {
+        ImportResponseDTO responseDTO = new ImportResponseDTO();
+        responseDTO.setId(importData.getId());
+        responseDTO.setAmount(importData.getAmount());
+        responseDTO.setCityExit(importData.getCityExit().name());
+        responseDTO.setCityEntry(importData.getCityEntry().name());
+        responseDTO.setLat(importData.getLat());
+        responseDTO.setLng(importData.getLng());
+        return responseDTO;
     }
 }
--- backend/src/main/java/com/backend/service/InfectionService.java
+++ backend/src/main/java/com/backend/service/InfectionService.java
@@ -1,11 +1,14 @@
 package com.backend.service;
 
+import com.backend.dto.InfectionRequestDTO;
+import com.backend.dto.InfectionResponseDTO;
 import com.backend.model.InfectionRecord;
 import com.backend.repository.InfectionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +16,34 @@ public class InfectionService {
 
     private final InfectionRepository infectionRepository;
 
-    public InfectionRecord addInfection(InfectionRecord infection) {
-        return infectionRepository.save(infection);
+    public InfectionResponseDTO addInfection(InfectionRequestDTO infectionRequestDTO) {
+        InfectionRecord infection = new InfectionRecord();
+        infection.setLat(infectionRequestDTO.getLat());
+        infection.setLng(infectionRequestDTO.getLng());
+        infection.setRadius(infectionRequestDTO.getRadius());
+        infection.setDisease(infectionRequestDTO.getDisease());
+        infection.setCases(infectionRequestDTO.getCases());
+        infection.setRegion(infectionRequestDTO.getRegion());
+
+        InfectionRecord savedInfection = infectionRepository.save(infection);
+        return convertToResponseDTO(savedInfection);
     }
 
-    public List<InfectionRecord> getAllInfections() {
-        return infectionRepository.findAll();
+    public List<InfectionResponseDTO> getAllInfections() {
+        return infectionRepository.findAll().stream()
+                .map(this::convertToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    private InfectionResponseDTO convertToResponseDTO(InfectionRecord infection) {
+        InfectionResponseDTO responseDTO = new InfectionResponseDTO();
+        responseDTO.setId(infection.getId());
+        responseDTO.setLat(infection.getLat());
+        responseDTO.setLng(infection.getLng());
+        responseDTO.setRadius(infection.getRadius());
+        responseDTO.setDisease(infection.getDisease());
+        responseDTO.setCases(infection.getCases());
+        responseDTO.setRegion(infection.getRegion());
+        return responseDTO;
     }
 }
