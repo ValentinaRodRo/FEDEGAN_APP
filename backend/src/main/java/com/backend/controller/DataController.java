a49abb80e4898179a59bef0632b43c3828cbfff7
arizaxel16 <arizaxel16@gmail.com>
2025-05-14 10:01:37 -0500

    Update controller to use dtos

--- backend/src/main/java/com/backend/controller/DataController.java
+++ backend/src/main/java/com/backend/controller/DataController.java
@@ -1,7 +1,9 @@
 package com.backend.controller;
 
-import com.backend.model.ImportRecord;
-import com.backend.model.InfectionRecord;
+import com.backend.dto.ImportRequestDTO;
+import com.backend.dto.ImportResponseDTO;
+import com.backend.dto.InfectionRequestDTO;
+import com.backend.dto.InfectionResponseDTO;
 import com.backend.service.ImportService;
 import com.backend.service.InfectionService;
 import lombok.RequiredArgsConstructor;
@@ -20,26 +22,26 @@ public class DataController {
     private final InfectionService infectionService;
 
     @PostMapping("/imports")
-    public ResponseEntity<ImportRecord> addImportData(@RequestBody ImportRecord importData) {
-        ImportRecord savedImport = importService.addImport(importData);
+    public ResponseEntity<ImportResponseDTO> addImportData(@RequestBody ImportRequestDTO importRequestDTO) {
+        ImportResponseDTO savedImport = importService.addImport(importRequestDTO);
         return new ResponseEntity<>(savedImport, HttpStatus.CREATED);
     }
 
     @GetMapping("/imports")
-    public ResponseEntity<List<ImportRecord>> getAllImportData() {
-        List<ImportRecord> imports = importService.getAllImports();
+    public ResponseEntity<List<ImportResponseDTO>> getAllImportData() {
+        List<ImportResponseDTO> imports = importService.getAllImports();
         return new ResponseEntity<>(imports, HttpStatus.OK);
     }
 
     @PostMapping("/infections")
-    public ResponseEntity<InfectionRecord> addInfectionData(@RequestBody InfectionRecord infection) {
-        InfectionRecord savedInfection = infectionService.addInfection(infection);
+    public ResponseEntity<InfectionResponseDTO> addInfectionData(@RequestBody InfectionRequestDTO infectionRequestDTO) {
+        InfectionResponseDTO savedInfection = infectionService.addInfection(infectionRequestDTO);
         return new ResponseEntity<>(savedInfection, HttpStatus.CREATED);
     }
 
     @GetMapping("/infections")
-    public ResponseEntity<List<InfectionRecord>> getAllInfectionData() {
-        List<InfectionRecord> infections = infectionService.getAllInfections();
+    public ResponseEntity<List<InfectionResponseDTO>> getAllInfectionData() {
+        List<InfectionResponseDTO> infections = infectionService.getAllInfections();
         return new ResponseEntity<>(infections, HttpStatus.OK);
     }
 }
